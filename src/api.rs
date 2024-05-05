67d1eee9e953cb86be575f13a976b588d12db831
Arthur Ryzhak <ryzhakar@gmail.com>
2024-05-06 01:41:54 +0300

    refactor: use blocking requests

--- src/api.rs
+++ src/api.rs
@@ -1,30 +1,112 @@
 use crate::models::{ClientInfo, StatementItem};
-use std::option::Option;
-use reqwest;
+use std::{thread, time::Duration};
+use reqwest::blocking::Client;
 use tracing;
+use rand::Rng;
 
 const MONOBANK_API_URL: &str = "https://api.monobank.ua/personal";
+const MAX_TIME_DIFF_SEC: u32 = 31 * 24 * 60 * 60;  // 31 days
+pub const WAIT_TIME_SEC: u32 = 60;
+pub const WAIT_JITTER_SEC: u32 = 5;
 
-pub fn get_client() -> reqwest::Client {
-    reqwest::Client::new()
+#[derive(Debug)]
+pub struct FetchingStatementsIterator<'a> {
+    pub client: &'a Client,
+    pub token: String,
+    pub account_id: String,
+    pub last_success_time: u32,
+    pub end_time: u32,
+    pub wait_length_sec: u32,
+    pub wait_jitter_sec: u32,
+}
+
+impl<'a> FetchingStatementsIterator<'a> {
+
+    fn calculate_next_window(&self) -> (u32, u32) {
+        let start = self.last_success_time + 1;
+        let end = std::cmp::min(start + MAX_TIME_DIFF_SEC, self.end_time);
+        (start, end)
+    }
+
+
+    fn sleep_with_jitter(&self) {
+        let jitter = rand::thread_rng().gen_range(0..self.wait_jitter_sec*1000);
+        let sleep_time = {
+            Duration::from_secs(self.wait_length_sec as u64)
+            + Duration::from_millis(jitter as u64)
+        };
+        thread::sleep(sleep_time);
+    }
+
+
+    fn fetch_next_batch(
+        &self,
+        start: u32,
+        end: u32,
+    ) -> Result<Vec<StatementItem>, reqwest::Error> {
+        self.sleep_with_jitter();
+        fetch_statements(
+            self.client,
+            &self.account_id,
+            start,
+            end,
+            &self.token
+        )
+    }
+
+    fn try_fetch(
+        &self,
+        start: u32,
+        end: u32,
+    ) -> Result<Vec<StatementItem>, reqwest::Error> {
+        let result = self.fetch_next_batch(start, end);
+        match result {
+            Ok(data) if data.len() == 500 => {
+                let delta = end - start;
+                if delta < 2 {
+                    return Ok(data)
+                };
+                self.try_fetch(start, start + delta / 2)
+            },
+            _ => result
+        }
+    }
+}
+impl<'a> Iterator for FetchingStatementsIterator<'a> {
+    type Item = Result<Vec<StatementItem>, reqwest::Error>;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.last_success_time >= self.end_time {
+            return None
+        }
+
+        let (start, end) = self.calculate_next_window();
+        match self.try_fetch(start, end) {
+            Ok(data) => {
+                self.last_success_time = std::cmp::min(end, self.end_time);
+                Some(Ok(data))
+            },
+            Err(e) => Some(Err(e))
+        }
+    }
 }
 
 
-pub async fn fetch_client_info(
-    client: &reqwest::Client,
+pub fn fetch_client_info(
+    client: &Client,
     token: &str,
 ) -> Result<ClientInfo, reqwest::Error> {
     let url = format!("{}/client-info", MONOBANK_API_URL);
     tracing::info!(token=token, "Getting client data...");
-    let response = client.get(url).header("X-Token", token).send().await?;
+    let response = client.get(url).header("X-Token", token).send()?;
     tracing::debug!("Deserializing client data...");
-    let client_info = response.json::<ClientInfo>().await?;
+    let client_info = response.json::<ClientInfo>()?;
     Ok(client_info)
 }
 
 
-pub async fn fetch_statements(
-    client: &reqwest::Client,
+pub fn fetch_statements(
+    client: &Client,
     resource_id: &str,
     from: u32,
     to: u32,
@@ -44,8 +126,8 @@ pub async fn fetch_statements(
         token = token,
         "Getting statements...",
     );
-    let response = client.get(&url).header("X-Token", token).send().await?;
+    let response = client.get(&url).header("X-Token", token).send()?;
     tracing::debug!("Deserializing statements...");
-    let statement_items = response.json::<Vec<StatementItem>>().await?;
+    let statement_items = response.json::<Vec<StatementItem>>()?;
     Ok(statement_items)
 }
--- src/main.rs
+++ src/main.rs
@@ -2,20 +2,20 @@ mod config;
 mod models;
 mod api;
 mod logger;
+use reqwest::blocking::Client;
 use std::time::{SystemTime, UNIX_EPOCH};
 
 
-#[tokio::main]
-async fn main() {
+fn main() {
     logger::initialize_logging();
     config::load_env();
-    let client = api::get_client();
+    let client = Client::new();
     let tokens = config::get_multiple_monobank_tokens();
-    let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs();
-    let before = now - 24 * 60 * 31;
+    let now = SystemTime::now().duration_since(UNIX_EPOCH).unwrap().as_secs() as u32;
+    let before: u32 = now - 24 * 60 * 31 * 2;
     for token_index in 0..tokens.len() {
         let token = &tokens[token_index];
-        let client_info = api::fetch_client_info(&client, &token).await.unwrap();
+        let client_info = api::fetch_client_info(&client, &token).unwrap();
         println!("{:?}", client_info);
         let proper_type = "black".to_string();
         let proper_currency = 980;
@@ -25,14 +25,17 @@ async fn main() {
                 acc.account_type == proper_type
                 && acc.currency_code == proper_currency
             ).unwrap();
-        let statements = api::fetch_statements(
-            &client,
-            &main_card.id,
-            before as u32,
-            now as u32,
-            &token
-        ).await.unwrap();
-        let _: Vec<_> = statements.into_iter()
-            .map(move |stat| { println!("{:?}", stat); }).collect();
+        let black_statements = api::FetchingStatementsIterator {
+            client: &client,
+            token: token.to_string(),
+            account_id: main_card.id.clone(),
+            last_success_time: before - 1,
+            end_time: now,
+            wait_length_sec: api::WAIT_TIME_SEC,
+            wait_jitter_sec: api::WAIT_JITTER_SEC,
+        };
+        for statement in black_statements {
+            println!("{:?}", statement);
+        }
     }
 }
