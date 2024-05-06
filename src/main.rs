93233f6324b373242944f1e857e499cc24254e27
Arthur Ryzhak <ryzhakar@gmail.com>
2024-05-06 22:26:26 +0300

    feat: check all cards

--- src/main.rs
+++ src/main.rs
@@ -17,25 +17,19 @@ fn main() {
         let token = &tokens[token_index];
         let client_info = api::fetch_client_info(&client, &token).unwrap();
         println!("{:?}", client_info);
-        let proper_type = "black".to_string();
-        let proper_currency = 980;
-        let main_card = client_info.clone().accounts.into_iter()
-            .find(
-                |acc|
-                acc.account_type == proper_type
-                && acc.currency_code == proper_currency
-            ).unwrap();
-        let black_statements = api::FetchingStatementsIterator {
-            client: &client,
-            token: token.to_string(),
-            account_id: main_card.id.clone(),
-            last_success_time: before - 1,
-            end_time: now,
-            wait_length_sec: api::WAIT_TIME_SEC,
-            wait_jitter_sec: api::WAIT_JITTER_SEC,
-        };
-        for statement in black_statements {
-            println!("{:?}", statement);
+        for card in client_info.accounts {
+            let card_statements = api::FetchingStatementsIterator {
+                client: &client,
+                token: token.to_string(),
+                account_id: card.id,
+                last_success_time: before - 1,
+                end_time: now,
+                wait_length_sec: api::WAIT_TIME_SEC,
+                wait_jitter_sec: api::WAIT_JITTER_SEC,
+            };
+            for statement in card_statements {
+                println!("{:?}", statement);
+            }
         }
     }
 }
